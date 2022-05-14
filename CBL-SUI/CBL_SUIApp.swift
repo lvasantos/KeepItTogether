cf0ad9a648973fe7a0d1a6a3f6ec541ff421f15f
Luciana Adriao <luicianaasantos@hotmail.com>
2022-05-14 18:10:37 -0300

    Initial Commit

---
+++ CBL-SUI/CBL_SUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CBL_SUIApp.swift
+//  CBL-SUI
+//
+//  Created by Luciana Adrião on 14/05/22.
+//
+
+import SwiftUI
+
+@main
+struct CBL_SUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CBL-SUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CBL-SUI
+//
+//  Created by Luciana Adrião on 14/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
